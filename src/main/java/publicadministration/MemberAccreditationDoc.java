cf9376894766a4134eb4d8721c90e5328ba255bb
Pablo Fraile Alonso <pablofrailealonso@gmail.com>
2021-12-28 17:06:34 +0100

    Implement MemberAccreditationDoc and his tests.
    This fixes the issue #6. We've to check the requireNonNull lines on the
    issue #11.

--- src/main/java/publicadministration/MemberAccreditationDoc.java
+++ src/main/java/publicadministration/MemberAccreditationDoc.java
@@ -3,15 +3,27 @@ package publicadministration;
 import data.AccreditationNumb;
 import data.Nif;
 
+import java.util.Objects;
+
 public class MemberAccreditationDoc extends PDFDocument {
+
     // Represents the member accreditation document
     private final Nif nif;
-    private final AccreditationNumb numAffil;
+    private final AccreditationNumb affiliationNumber;
 
     // Initialize attributes
-    public MemberAccreditationDoc(Nif nif, AccreditationNumb nAff) {
-        throw new RuntimeException("TODO");
+    public MemberAccreditationDoc(Nif nif, AccreditationNumb affiliationNumber) {
+        Objects.requireNonNull(nif, "Nif can't be null");
+        Objects.requireNonNull(affiliationNumber, "Affiliation Number can't be null");
+        this.nif = nif;
+        this.affiliationNumber = affiliationNumber;
     }
 
-    // TODO: The getters
+    public Nif getNif() {
+        return nif;
+    }
+
+    public AccreditationNumb getAffiliationNumber() {
+        return affiliationNumber;
+    }
 }
---
+++ src/test/java/publicadministration/MemberAccreditationDocTest.java
@@ -0,0 +1,52 @@
+package publicadministration;
+
+import data.AccreditationNumb;
+import data.Nif;
+import exceptions.BadFormatAccreditationNumberException;
+import exceptions.BadFormatNifException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class MemberAccreditationDocTest {
+
+    @Test
+    public void nullNif() {
+        Throwable exception =
+                assertThrows(
+                        NullPointerException.class,
+                        () -> {
+                            new MemberAccreditationDoc(null, new AccreditationNumb("123"));
+                        });
+        assertEquals("Nif can't be null", exception.getMessage());
+    }
+
+    @Test
+    public void nullAccreditationNumber() {
+        Throwable exception =
+                assertThrows(
+                        NullPointerException.class,
+                        () -> {
+                            new MemberAccreditationDoc(new Nif("49263972L"), null);
+                        });
+        assertEquals("Affiliation Number can't be null", exception.getMessage());
+    }
+
+    @Test
+    public void sameNifReference() throws BadFormatNifException, BadFormatAccreditationNumberException {
+        Nif nif = new Nif("49263972L");
+        MemberAccreditationDoc memberAccreditationDoc =
+                new MemberAccreditationDoc(nif, new AccreditationNumb("123"));
+        assertEquals(nif, memberAccreditationDoc.getNif());
+    }
+
+
+    @Test
+    public void sameAccreditationNumberReference() throws BadFormatNifException, BadFormatAccreditationNumberException {
+        AccreditationNumb accreditationNumb = new AccreditationNumb("123");
+        MemberAccreditationDoc memberAccreditationDoc =
+                new MemberAccreditationDoc(new Nif("49263972L"), accreditationNumb);
+        assertEquals(accreditationNumb, memberAccreditationDoc.getAffiliationNumber());
+    }
+}
