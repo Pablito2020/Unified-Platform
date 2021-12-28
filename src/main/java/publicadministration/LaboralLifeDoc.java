a86cedf7e6ca3120463061142155f77c97b1ca81
Oscars35 <oscar13785@gmail.com>
2021-12-28 11:23:27 +0100

    Comments deleted
    I deleted the comments on the code

--- src/main/java/publicadministration/LaboralLifeDoc.java
+++ src/main/java/publicadministration/LaboralLifeDoc.java
@@ -2,12 +2,10 @@ package publicadministration;
 
 import data.Nif;
 
-// Represents the laboral life
 public class LaboralLifeDoc extends PDFDocument {
     private final Nif nif;
     private final QuotePeriodsCollection quotePds;
 
-    // Initialize attributes
     public LaboralLifeDoc(Nif nif, QuotePeriodsCollection qtP) {
         this.nif = nif;
         this.quotePds = qtP;
